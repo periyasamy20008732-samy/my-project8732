31191ba354c7b42a76c41059c4188cb5b9c09016
unnikochu <unnikrishnan2012@gmail.com>
2025-05-13 22:44:55 +0530

    issues solved in the local system

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
@@ -14,8 +13,4 @@ Route::get('/', function () {
 
 Route::get('/admin', function () {
     return view('admin.auth.login');
-=======
-Route::get('/', function () {
-    return view('welcome');
->>>>>>> efb3858ceaa94a5283bbc7d05946b724a55cab23
 });
